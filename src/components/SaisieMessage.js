86ea988e09442d1c3b90742447f9c5ba117adff6
Yannick COUSIN <94236356+yannick-cousin@users.noreply.github.com>
2021-12-22 12:41:56 +0100

    Little Update of Messages

---
+++ src/components/SaisieMessage.js
@@ -0,0 +1,16 @@
+import './SaisieMessage.css'
+
+const SaisieMessage = ({setContenueMessage, contenueMessage}) => {
+/*
+  const alerte = () => {
+    alert("Faudra penser à payer l'abonement d'abord");
+  }
+*/
+  return (
+    <div className="saisiemessage">
+      <div className="envoi">Accepter la demande de tchat</div>
+    </div>
+  )
+}
+
+export default SaisieMessage;
